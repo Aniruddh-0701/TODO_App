11ac92b60f0a32fa1de568868146b102da264965
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2020-11-23 14:12:43 +0530

    Update

--- app.py
+++ app.py
@@ -1,49 +1,10 @@
-from flask import Flask, render_template, request
-from models import Schema
-import cgi
-from Srevice import ToDoService
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return render_template('F1.html')
-
-
-@app.route('/', methods=["POST"])
-def res():
-    global c
-    c = 0
-    s = request.form['Select']
-
-    if s == "New":
-        c += 1
-        return render_template('Fe.html', s=s)
-    if c == 0:
-        return render_template('pass.html', s=s)
-    else:
-        req()
-
-
-def req():
-    s = "Sent"
-    text = request.form['text']
-    description = request.form['description']
-    ddate = request.form['ddate']
-    c(s, text, description, ddate)
-
-
 @app.route('/')
-def c(s, text, description, ddate):
-    return render_template('pass.html', s=s, t=text, de=description, dd=ddate)
+def index():
+    return 'Hello World'
 
-# l = [text, description, ddate]
-# params = {'text': text, 'description': description, 'ddate': ddate}
-# return ToDoService().create(params)
-
-
-if __name__ == "__main__":
-    Schema()
-
+if __name__=='__main__':
     app.run(debug=True)
