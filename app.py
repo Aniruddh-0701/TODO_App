748b752e28d62bafcff78b54204dbf2d5e0aee48
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2020-11-24 23:47:01 +0530

    Added Login and Register

--- app.py
+++ app.py
@@ -1,35 +1,94 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from markupsafe import escape
 from datetime import datetime
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
+    __tablename__ = 'Tasks'
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
     completed = db.Column(db.Integer, default = 0) # Currently unused
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
     deadline = db.Column(db.DateTime)
+    user = db.Column(db.String(100), nullable = False)
 
     def __repr(self):
         return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+class Users(db.Model):
+    __tablename__ = 'Users'
+    _id = db.Column(db.String(50), primary_key = True)
+    username = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(16), nullable = False)
+
+@app.route('/')
 def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        uid = request.form['uid']
+        try:
+            user = Users.query.filter_by(_id = uid).first()
+            session['username'] = user['username']    
+            # return redirect(url_for('tasks'))
+        except:
+            session['username'] = 'GUEST'
+        finally: return redirect(url_for('tasks'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/registration')
+def load():
+    return redirect(url_for('register'))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        uid = request.form['uid']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        pwd = request.form['pwd']
+        new_user = Users(_id = uid, username = fname+' '+lname, password = pwd)
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    else: 
+        return render_template('register.html')
+
+@app.route('/tasks', methods=['POST', 'GET'])
+def tasks():
     if request.method == 'POST':
         task_txt = request.form['task']
         task_ddl = datetime.strptime(request.form['deadline'], r'%Y-%m-%d')
-        newTask = Todo(content = task_txt, deadline = task_ddl)
+        user = session['username']
+        newTask = Todo(content = task_txt, deadline = task_ddl, user = user)
 
         db.session.add(newTask)
         db.session.commit()
-        return redirect('/') 
+        return redirect('/tasks') 
             
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        try:
+            tasks = Todo.query.order_by(Todo.date_created).all()
+            return render_template('tasks.html', tasks=tasks)
+        except: 
+            return render_template('tasks.html')
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -38,7 +97,7 @@ def delete(id):
     try:
         db.session.delete(taskDelete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/tasks')
     except:
         return 'An error was encountered'
 
--- db_manager.py
+++ db_manager.py
@@ -3,4 +3,22 @@ engine = create_engine('sqlite:///test.db', echo = True)
 
 meta = MetaData()
 
-t = text('ALTER TABLE Cats ADD COLUMN DOB;')
+conn = engine.connect()
+
+# tasks = text(f'CREATE TABLE Tasks ('\
+#     f'id INTEGER NOT NULL,'\
+#     f'content VARCHAR(200) NOT NULL,'\
+#     f'completed INTEGER,'\
+#     f'date_created DATETIME,'\
+#     f'deadline DATETIME,'\
+#     f'user VARCHAR(200) NOT NULL)'\
+#     )
+
+users = text(f'CREATE TABLE Users ('\
+    f'_id VARCHAR(50) NOT NULL,'\
+    f'username VARCHAR(200) NOT NULL,'\
+    f'password VARCHAR(200) NOT NULL)'
+    )
+
+# print(conn.execute(tasks))
+print(conn.execute(users))
