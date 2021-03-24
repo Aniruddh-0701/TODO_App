978df7c9a96f0c36f8d56d55cfab78bf56383247
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2021-03-24 17:03:27 +0530

    Deleted unwanted files

--- Srevice.py
+++
@@ -1,9 +0,0 @@
-from models import ToDoModel
-
-
-class ToDoService:
-    def __init__(self):
-        self.model = ToDoModel()
-
-    def create(self, *params):
-        self.model.create(params[0], params[1], params[2])
