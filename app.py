345e1b2153b21631917cbf45d20c745d5823a3f0
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2021-07-06 15:49:28 +0530

    Update app.py
    Added User id duplicate check, type hints, server side form validations

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from os import urandom
-from threading import Timer
 import webbrowser
 from datetime import datetime
+from os import error, urandom
+from threading import Timer
 
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
 from waitress import serve
+from werkzeug.wrappers import Response
 
 from _pass import *
 
 # App init
 app = Flask(__name__)
 app.secret_key = urandom(16)  # Secret key
+
 # DB configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
 # Table manager classes
+
+# ToDo table Manager
 class Todo(db.Model):
     __tablename__ = "Tasks"
     id = db.Column(db.Integer, primary_key=True)
@@ -27,10 +31,11 @@ class Todo(db.Model):
     deadline = db.Column(db.DateTime)
     user = db.Column(db.String(100), nullable=False)
 
-    def __repr(self):
+    def __repr__(self) -> str:
         return "<Task %r>" % self.id
 
 
+# Users table manager
 class Users(db.Model):
     __tablename__ = "Users"
     _id = db.Column(db.String(50), primary_key=True)
@@ -38,7 +43,7 @@ class Users(db.Model):
     password = db.Column(db.String(16), nullable=False)
 
 
-# Start dir
+# Start directory
 @app.route("/")
 def index():
     if "username" in session:
@@ -46,25 +51,27 @@ def index():
     return redirect(url_for("login"))
 
 
-# Login
+# Login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
+    if request.method == "POST":  # verify login details
         uid = request.form["uid"]
         pwd = hash(request.form["pwd"])
         try:
             user = Users.query.filter_by(_id=uid).first()
             if check_password(user.password, pwd):
                 flash("Login successful", "success")
-                session["username"] = user.username
-                return redirect(url_for("tasks"))
+                session["username"] = user.username  # Set username on session
+                return redirect(
+                    url_for("tasks")
+                )  # redirect to tasks page on successful login
             else:
                 flash("InCorrect Username or Password", "error")
                 3 / 0
         except:
             flash("User does not exist", "error")
             return render_template("login.html")
-    return render_template("login.html")
+    return render_template("login.html")  # Loading Login page on GET method
 
 
 # Logout
@@ -72,7 +79,7 @@ def login():
 def logout():
     # remove the username from the session if it's there
     session.pop("username", None)
-    return redirect(url_for("index"))
+    return redirect(url_for("index"))  # redirect to index page
 
 
 # register
@@ -83,14 +90,39 @@ def register():
         userId = uid = request.form["uid"]
         fname = request.form["fname"]
         lname = request.form["lname"]
-        pwd = encrypt_password(request.form["pwd"])
-        new_user = Users(_id=uid, username=fname + " " + lname, password=pwd)
+        pwd = request.form["pwd"]
+        try:
+            assert Validator.validate_id(uid), "Invalid UID"
+            assert Validator.validate_name(fname), "Invalid Name"
+            assert Validator.validate_name(lname), "Invalid Name"
+            assert Validator.validate_pwd(pwd), "Invalid password"
+            epwd = encrypt_password(pwd)
 
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("login"))
+            new_user = Users(_id=uid, username=fname + " " + lname, password=epwd)
+
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("login"))
+        except AssertionError as err:
+            flash(err, "error")
+            return render_template(
+                "register.html", user_id=uid, fname=fname, lname=lname, pwd=pwd
+            )
+        except Exception:
+            flash("Duplicate entry", "error")
+            return render_template(
+                "register.html", user_id=uid, fname=fname, lname=lname, pwd=pwd
+            )
     else:
-        return render_template("register.html")
+        return render_template("register.html", user_id="", fname="", lname="", pwd="")
+
+
+# Check duplicate user id
+@app.route("/check-duplicate/<uid>", methods=["POST"])
+def check_duplicate(uid: str):
+    userUnique = Users.query.get_or_404(uid)
+    # print(userUnique)
+    return "Already Exists", 200
 
 
 # forgot password
@@ -109,10 +141,6 @@ def forgotPwd():
             return redirect(url_for("reset"))
         else:
             return f"User Name does not exist"
-        # try:
-
-        # except:
-        #     return render_template('login.html')
     else:
         return render_template("forgot-password.html")
 
@@ -124,8 +152,12 @@ def reset():
     if request.method == "POST":
         userId = request.form["uid"]
         newPwd = request.form["pwd"]
+        try:
+            assert Validator.validate_pwd(newPwd), "Invalid Password"
+        except AssertionError as err:
+            flash(err, "error")
         user = Users.query.filter_by(_id=userId).first()
-        user.password = newPwd
+        user.password = encrypt_password(newPwd)
         db.session.commit()
         return redirect(url_for("login"))
     else:
@@ -158,7 +190,7 @@ def tasks():
 
 # delete tasks
 @app.route("/delete/<int:id>")
-def delete(id):
+def delete(id: int):
     taskDelete = Todo.query.get_or_404(id)
 
     try:
@@ -171,7 +203,7 @@ def delete(id):
 
 # update tasks
 @app.route("/update/<int:id>", methods=["GET", "POST"])
-def update(id):
+def update(id: int):
     taskUpdate = Todo.query.get_or_404(id)
     if request.method == "POST":
         task_content = request.form["task"]
@@ -187,7 +219,7 @@ def update(id):
 
 # mark task as done
 @app.route("/mark-as-done/<int:id>")
-def markAsDone(id):
+def markAsDone(id: int):
     taskDone = Todo.query.get_or_404(id)
     taskDone.completed = 1
     db.session.commit()
@@ -196,19 +228,24 @@ def markAsDone(id):
 
 # mark task as pending
 @app.route("/mark-as-pending/<int:id>")
-def pending(id):
+def pending(id: int):
     taskPending = Todo.query.get_or_404(id)
     taskPending.completed = 0
     db.session.commit()
     return redirect(url_for("tasks"))
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 # Launch the app in browser
 def open_browser():
-    webbrowser.open("http://127.0.0.1:0701/", new=1, autoraise=True)
+    webbrowser.open("http://127.0.0.1:5000/", new=1, autoraise=True)
 
 
 if __name__ == "__main__":
-    Timer(1, open_browser).start()
-    # app.run(debug=True)
-    serve(app, listen='*:0701')
+    # Timer(1, open_browser).start()
+    app.run(debug=True)
+    # serve(app, listen="*:0701")
